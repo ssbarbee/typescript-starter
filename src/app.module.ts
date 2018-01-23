6eb5fe4d5cb997f20279ea7b72ae54b4d7b32607
Filip Bozinovski <ssbarbee@gmail.com>
2018-01-23 01:22:02 +0100

    Imported CatModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { CatsModule } from "./cats/cats.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  components: [],
+    imports: [CatsModule]
 })
-export class ApplicationModule {}
+export class ApplicationModule {
+}
