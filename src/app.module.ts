6fe18d8620a60867e562271f3f2a7dcf0125c460
Filip Bozinovski <ssbarbee@gmail.com>
2018-01-23 01:48:58 +0100

    Added filters to app filter via useGlobalFilters.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,17 @@ import { MiddlewaresConsumer, Module, NestModule } from '@nestjs/common';
 import { CatsModule } from "./cats/cats.module";
 import { CatsController } from "./cats/cats.controller";
 import { LoggerMiddleware } from "./middlewares/logger.middleware";
+import { ExceptionsModule } from "./exceptions/exceptions.module";
+import { ExceptionsController } from "./exceptions/exceptions.controller";
 
 @Module({
-    imports: [CatsModule]
+    imports: [CatsModule, ExceptionsModule]
 })
 export class ApplicationModule implements NestModule {
     configure(consumer: MiddlewaresConsumer): void {
         consumer
         .apply(LoggerMiddleware)
         .with('CatsModule')
-        .forRoutes(CatsController);
+        .forRoutes(CatsController, ExceptionsController);
 }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
+import { AnyExceptionFilter } from "./filters/any-exception.filter";
+import { HttpExceptionFilter } from "./filters/http-exception.filter";
 
 async function bootstrap() {
     const app = await NestFactory.create(ApplicationModule);
-	await app.listen(3000);
+    app.useGlobalFilters(
+        new HttpExceptionFilter(),
+        new AnyExceptionFilter());
+    await app.listen(3000);
 }
+
 bootstrap();
